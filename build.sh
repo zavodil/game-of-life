f4e6172ec808ce8c2b8596c316a0c6ba456da46b
Zavodil <support@zavodil.ru>
2021-08-28 11:17:33 +0300

    Build script fix to autocreate missing folders

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+mkdir -p out
+mkdir -p neardev
 cp target/wasm32-unknown-unknown/release/game.wasm ./out/main.wasm
-
